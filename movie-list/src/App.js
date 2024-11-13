ced468e7a3360bd8b9d5853acbe18b8d0050cacf
nedrazzz <smg9972@gmail.com>
2024-11-13 18:26:36 +0900

    feat: display movies from apis

--- movie-list/src/App.js
+++ movie-list/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movieList, setMovieList] = useState([]);
+
+  const loadMovies = async () => {
+    const response = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5`)).json();
+    setMovieList(response.data.movies);
+    setIsLoading(false);
+  }
+  useEffect(() => {
+    loadMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLoading ?
+        <h1>Loading ...</h1>
+        :
+        <div>
+          {movieList.map(movie => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt='movie cover'></img>
+              <h3>{movie.title}</h3>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map(genre => (<li>{genre}</li>))}
+              </ul>
+            </div>
+          ))}
+        </div>}
     </div>
   );
 }
